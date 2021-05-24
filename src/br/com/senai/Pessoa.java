a810e5a7f72a5ce81853b40387ce05650aa7ef1a
EduardoAraujo047 <you@example.com>
2021-05-24 16:08:51 -0300

    Classe pessoa

---
+++ src/br/com/senai/Pessoa.java
@@ -0,0 +1,31 @@
+package br.com.senai;
+
+public class Pessoa {
+	
+	private String nome;
+	private int anoDeNascimento;
+	private String  sexo;
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public void setAnoDeNascimento(int anoDeNascimento) {
+		this.anoDeNascimento = anoDeNascimento;
+	}
+	public int getAnoDeNascimento() {
+		return anoDeNascimento;
+	}
+	
+	public String getSexo() {
+		return sexo;
+	}
+	
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}	
+}
