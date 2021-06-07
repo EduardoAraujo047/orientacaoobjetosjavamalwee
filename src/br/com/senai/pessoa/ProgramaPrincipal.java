3dadc7781f08d7b083c920594e8b287aeaab3c60
EduardoAraujo047 <you@example.com>
2021-06-07 16:31:05 -0300

    Métodos de adição e listagem de dados do objeto pessoa

--- src/br/com/senai/pessoa/ProgramaPrincipal.java
+++ src/br/com/senai/pessoa/ProgramaPrincipal.java
@@ -1,10 +1,45 @@
 package br.com.senai.pessoa;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class ProgramaPrincipal {
 
 	public static void main(String[] args) {
-		
+		List<Pessoa> pessoas = new ArrayList<>();
 
+		PessoaController pessoaController = new PessoaController();
+
+		boolean sair = false;
+
+		do {
+			pessoaController.menu();
+
+			int opcao = pessoaController.leOpcao();
+
+			switch (opcao) {
+			case 1:
+				pessoas.add(pessoaController.cadastrarPessoa());
+				break;
+
+			case 2:
+				pessoaController.listarPessoas(pessoas);
+
+				break;
+
+			default:
+				System.out.println("Função inválida.");
+				break;
+
+			case 9:
+				sair = true;
+				break;
+			}
+
+		} while (!sair);
+
+		System.out.println("Sistema finalizado.");
 	}
 
 }
